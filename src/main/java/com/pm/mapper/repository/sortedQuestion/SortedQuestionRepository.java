dac74025ee47580735b2e6f2d25409a0664b66c7
WickiesLee <crowdrider1@gmail.com>
2019-04-03 12:47:44 +0900

    HOT 결과 수정
    완료

--- src/main/java/com/pm/mapper/repository/sortedQuestion/SortedQuestionRepository.java
+++ src/main/java/com/pm/mapper/repository/sortedQuestion/SortedQuestionRepository.java
@@ -12,7 +12,8 @@ import java.util.List;
 @Repository
 public interface SortedQuestionRepository extends JpaRepository<SortedQuestion, Long> {
     //현재 날짜에서 7일 이내에의 게시물에서 views의 내림차순으로 정렬하여 조회한다.
-    @Query(value = "select * from mapper.sorted_question s where date > (NOW() - INTERVAL 7 day ) order by views desc limit 10 ", nativeQuery = true)
+//    @Query(value = "select * from mapper.sorted_question s where date > (NOW() - INTERVAL 7 day ) order by views desc limit 10 ", nativeQuery = true)
+    @Query(value = "select * from mapper.sorted_question s where date > (NOW() - INTERVAL 30 day ) order by views desc limit 10 ", nativeQuery = true)
     List<SortedQuestion> findAllDateAfter7Day();
 
     Page<SortedQuestion> findAllByTagsContainsIgnoreCase(String keyword, Pageable pageable);
